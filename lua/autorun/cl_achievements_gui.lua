1deaebceaab8efd66c7d9ba88a1046aaea06131a
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2016-05-28 13:01:52 +1000

    Changed a bunch of things
    I decided to rewrite most of the SetPos and SetSize bits so they would work with docking instead, which also makes the main panel able to be resized now, so I enabled resizing on line 17 because of this. I also changed the paint functions a little, and moved some of the code around, and I removed the scrollpanel that houses the two DListViews since DListViews have their own scrollpanel.

--- lua/autorun/cl_achievements_gui.lua
+++ lua/autorun/cl_achievements_gui.lua
@@ -7,171 +7,142 @@ if CLIENT then
 	
 	function OpenAchievementGUI()
 
-		if main then return end
+		if IsValid( main ) then return end
 				
 		main = vgui.Create( "DFrame" )
 		main:SetSize( 1024, 400 )
 		main:SetTitle( "" )
-		main:SetVisible( true )
-		main:ShowCloseButton( true )
 		main:MakePopup()
 		main:Center()
-		main.btnMaxim:Hide()
-		main.btnMinim:Hide() 
-		main.btnClose:Hide()
-		main.Paint = function()
+		main:SetSizable( true )
+		main:ShowCloseButton( false )
+		main:SetDeleteOnClose( true )
+		main:DockPadding( 8, 24+5, 8, 8 )
+		main.Paint = function( self, w, h )
 			surface.SetDrawColor( 50, 50, 50, 135 )
-			surface.DrawOutlinedRect( 0, 0, main:GetWide(), main:GetTall() )
+			surface.DrawOutlinedRect( 0, 0, w, h )
 			surface.SetDrawColor( 2, 134, 242, 240 )
-			surface.DrawRect( 1, 1, main:GetWide() - 2, main:GetTall() - 2 )
+			surface.DrawRect( 1, 1, w - 2, h - 2 )
 			surface.SetFont( "bold" )
-			surface.SetTextPos( main:GetWide() / 2 - surface.GetTextSize( "Achievements" ) / 2, 6 ) 
+			surface.SetTextPos( w / 2 - surface.GetTextSize( "Achievements" ) / 2, 6 ) 
 			surface.SetTextColor( 255, 255, 255, 255 )
 			surface.DrawText( "Achievements" )
 		end
 		
 		local close = vgui.Create( "DButton", main )
-		close:SetPos( main:GetWide() - 50, 0 )
-		close:SetSize( 44, 22 )
 		close:SetText( "" )
 				
 		local colorv = Color( 150, 150, 150, 250 )
-		function PaintClose()
-			if not main then return end
+		
+		close.Paint = function( self, w, h )
 			surface.SetDrawColor( colorv )
-			surface.DrawRect( 1, 1, close:GetWide() - 2, close:GetTall() - 2 )	
+			surface.DrawRect( 1, 1, w - 2, h - 2 )	
 			surface.SetFont( "bold_x" )
+			local tw, th = surface.GetTextSize( "x" ) -- the text height is wrong but the width is usable
 			surface.SetTextColor( 255, 255, 255, 255 )
-			surface.SetTextPos( 17, 0.5 ) 
+			surface.SetTextPos( (w/2)-(tw/2), 0.5 )
 			surface.DrawText( "x" )
 			return true
 		end
 		
-		close.Paint = PaintClose		
-		close.OnCursorEntered = function()
-			colorv = Color( 195, 75, 0, 250 )
-			PaintClose()
-		end	
-		
-		close.OnCursorExited = function()
-			colorv = Color( 150, 150, 150, 250 )
-			PaintClose()
-		end	
-		
-		close.OnMousePressed = function()
-			colorv = Color( 170, 0, 0, 250 )
-			PaintClose()
-		end	
-		
+		close.OnCursorEntered = function() colorv = Color( 195, 75, 0, 250 ) end
+		close.OnCursorExited = function() colorv = Color( 150, 150, 150, 250 ) end
+		close.OnMousePressed = function() colorv = Color( 170, 0, 0, 250 ) end
 		close.OnMouseReleased = function()
 			if not LocalPlayer().InProgress then
 				main:Close()
 			end
-		end	
-		
-		main.OnClose = function()
-			main:Remove()
-			if main then
-				main = nil
-			end
-		end	
+		end
 		
 		local inside = vgui.Create( "DPanel", main )
-		inside:SetPos( 7, 27 )
-		inside:SetSize( main:GetWide() - 14, main:GetTall() - 34 )
-		inside.Paint = function()
+		inside:Dock( FILL )
+		inside:DockPadding( 5, 5, 5, 5 )
+		inside:InvalidateParent( true )
+		inside.Paint = function( self, w, h )
 			surface.SetDrawColor( 255, 255, 255, 255 )
-			surface.DrawOutlinedRect( 0, 0, inside:GetWide(), inside:GetTall() )
+			surface.DrawOutlinedRect( 0, 0, w, h )
 			surface.SetDrawColor( 255, 255, 255, 250 )
-			surface.DrawRect( 1, 1, inside:GetWide() - 2, inside:GetTall() - 2 )
+			surface.DrawRect( 1, 1, w - 2, h - 2 )
 		end
 		
 		local panel_left = vgui.Create("DPanel", inside)
-		panel_left:SetPos(5,5)
-		panel_left:SetSize(615, 355)
+		panel_left:Dock( LEFT )
 		panel_left:SetBackgroundColor(Color(255,255,255))
+		panel_left:DockPadding( 5, 32 + 5, 5, 5 )
+		panel_left:SetWide( inside:GetWide()/3*2 - 8 )
+		panel_left.PaintOver = function( self, w, h )
+			surface.SetFont( "bold" )
+			surface.SetTextColor( 0, 0, 0, 255 )
+			local tw, th = surface.GetTextSize( "Locked" )
+			surface.SetTextPos( (w/4)-(tw/2), th/2 )
+			surface.DrawText( 'Locked' )
+			local tw2, th2 = surface.GetTextSize( "Unlocked" )
+			surface.SetTextPos( ((w/4)*3)-(tw2/2), th2/2 )
+			surface.DrawText( 'Unlocked' )
+		end
 		
 		local panel_right = vgui.Create("DPanel", inside)
-		panel_right:SetPos(625,5)
-		panel_right:SetSize(380, 355)
+		panel_right:Dock( RIGHT )
+		panel_right:DockPadding( 8, 8, 8, 8 )
 		panel_right:SetBackgroundColor(Color(255,255,255))
 		
-		local achievement_name = vgui.Create("DLabel", panel_right)
-		achievement_name:SetPos(5,5)
+		local scroll = vgui.Create("DScrollPanel", panel_right)
+		scroll:Dock( FILL )
+		
+		local achievement_name = vgui.Create("DLabel", scroll)
+		achievement_name:Dock( TOP )
+		achievement_name:SetWrap( true )
+		achievement_name:SetFont( "bold" )
 		achievement_name:SetText("")
+		achievement_name:SetTextColor(Color(0,0,0))
 		
-		local achievement_desc = vgui.Create("DLabel", panel_right)
-		achievement_desc:SetPos(5,30)
+		local achievement_desc = vgui.Create("DLabel", scroll)
+		achievement_desc:Dock( TOP )
+		achievement_desc:SetWrap( true )
+		achievement_desc:SetFont( "normal" )
 		achievement_desc:SetText("")
+		achievement_desc:SetTextColor(Color(0,0,0))
 		
-		local locked_lbl = vgui.Create("DLabel", panel_left)
-		surface.SetFont("bold")
-		local locked_size = surface.GetTextSize("Locked")
-		locked_lbl:SetPos(150 - (locked_size / 2), 10)
-		locked_lbl:SetFont("bold")
-		locked_lbl:SetTextColor(Color(0,0,0))
-		locked_lbl:SetText("Locked")
-		locked_lbl:SizeToContents()
-		
-		local unlocked_lbl = vgui.Create("DLabel", panel_left)
-		surface.SetFont("bold")
-		local unlocked_size = surface.GetTextSize("Unlocked")
-		unlocked_lbl:SetPos(460 - (unlocked_size / 2), 10)
-		unlocked_lbl:SetFont("bold")
-		unlocked_lbl:SetTextColor(Color(0,0,0))
-		unlocked_lbl:SetText("Unlocked")
-		unlocked_lbl:SizeToContents()
-		
-		local scroll = vgui.Create("DScrollPanel", panel_left);
-		scroll:SetPos(5,35)
-		scroll:SetSize(605,315)
-		
-		local achievement_lock_list = vgui.Create("DListView", scroll)
+		local achievement_lock_list = vgui.Create("DListView", panel_left)
 		achievement_lock_list:SetMultiSelect(false)
 		achievement_lock_list:AddColumn("Achievement Name")
-		achievement_lock_list:SetSize(300,315)
+		achievement_lock_list:Dock( LEFT )
 		
-		local achievement_unlock_list = vgui.Create("DListView", scroll)
+		achievement_lock_list.OnRowSelected = function(self, line)
+			for _, achievement in pairs(Achievements.List) do
+				if self:GetLine(line):GetValue(1) == achievement.displayname then
+					achievement_name:SetText(achievement.displayname)
+					achievement_name:SizeToContents()
+					achievement_desc:SetText(achievement.description)
+					achievement_desc:SizeToContents()
+				end
+			end
+		end
+		
+		local achievement_unlock_list = vgui.Create("DListView", panel_left)
 		achievement_unlock_list:SetMultiSelect(false)
 		achievement_unlock_list:AddColumn("Achievement Name")
-		achievement_unlock_list:SetSize(300,315)
-		achievement_unlock_list:SetPos(305,0)
+		achievement_unlock_list:Dock( RIGHT )
 		
-		achievement_lock_list.OnRowSelected = function(panel, line)
+		achievement_unlock_list.OnRowSelected = achievement_lock_list.OnRowSelected
 		
-			for _,achievement in pairs(Achievements.List) do
-				if achievement_lock_list:GetLine(line):GetValue(1) == achievement.displayname then
-					achievement_name:SetFont("bold")
-					achievement_name:SetTextColor(Color(0,0,0))
-					achievement_name:SetText(achievement.displayname)
-					achievement_name:SizeToContents()
-					achievement_desc:SetFont("normal")
-					achievement_desc:SetTextColor(Color(0,0,0))
-					achievement_desc:SetText(achievement.description)
-					achievement_desc:SizeToContents()
-				end
-			end
+		main.PerformLayout = function( self )
 		
+			close:SetPos( self:GetWide() - 50, 0 )
+			close:SetSize( 44, 22 )
+			
+			panel_right:SetWide( inside:GetWide()/3 - 8 )
+			panel_left:SetWide( inside:GetWide()/3*2 - 8 )
+			
+			achievement_name:SizeToContents()
+			achievement_desc:SizeToContents()
+			
+			achievement_lock_list:SetWide( ( panel_left:GetWide()/2 )-8 )
+			achievement_unlock_list:SetWide( ( panel_left:GetWide()/2 )-8 )
+			
 		end
 		
-		achievement_unlock_list.OnRowSelected = function(panel, line)
-		
-			for _,achievement in pairs(Achievements.List) do
-				if achievement_unlock_list:GetLine(line):GetValue(1) == achievement.displayname then
-					achievement_name:SetFont("bold")
-					achievement_name:SetTextColor(Color(0,0,0))
-					achievement_name:SetText(achievement.displayname)
-					achievement_name:SizeToContents()
-					achievement_desc:SetFont("normal")
-					achievement_desc:SetTextColor(Color(0,0,0))
-					achievement_desc:SetText(achievement.description)
-					achievement_desc:SizeToContents()
-				end
-			end
-		
-		end
-		
-		for _,achievement in pairs(Achievements.List) do
+		for _, achievement in pairs(Achievements.List) do
 			if table.HasValue(Achievements.Completed, achievement.name) then
 				achievement_unlock_list:AddLine(achievement.displayname)
 			else
